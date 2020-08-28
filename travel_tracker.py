9d1afc594f42dab51fbee66c51e044804bad4f7c
rhys-simpson <rhys030102@gmail.com>
2020-08-28 16:03:52 +1000

    initial work on list menu option

--- travel_tracker.py
+++ travel_tracker.py
@@ -5,10 +5,13 @@ Date started: 24/08/2020
 GitHub URL:
 """
 
+FILENAME = "places.csv"
+
 
 def main():
+    """ """
     print("Travel Tracker 1.0 - by Rhys Simpson")
-    in_file = open("places.csv", "r")
+    in_file = open(FILENAME, "r")
     number = len(list(in_file))
     print("{} places loaded from places.csv".format(number))
     menu = "Menu:\nL - List Places\nA - Add new place\nM - Mark a place as visited\nQ - Quit"
@@ -16,16 +19,35 @@ def main():
     choice = input(">>>").upper()
     while choice != "Q":
         if choice == "L":
-            print("l")
+            places = get_places()
+            print_places(places)
         elif choice == "A":
             print("add")
         elif choice == "M":
             print("mark")
         else:
             print("Invalid Menu Choice")
-        print(menu)
-        choice = input(">>>").upper()
+        # print(menu)
+        # choice = input(">>>").upper()
     print("Have a nice day")
+
+
+def get_places():
+    places = []
+    in_file = open(FILENAME)
+    for line in in_file:
+        line = line.strip()
+        parts = line.split(",")
+        parts[2] = int(parts[2])
+        places.append(parts)
+    in_file.close()
+    return places
+
+
+def print_places(places):
+    """     """
+    for i in places:
+        print("{}. {} in {} priority {}".format(i, places[0], places[1], places[2]))
 
 
 if __name__ == '__main__':
