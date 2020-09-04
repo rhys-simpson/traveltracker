1a00b38bbd1ccff0968450bd17d20db61ce6da78
rhys-simpson <rhys030102@gmail.com>
2020-09-04 13:47:34 +1000

    fixed mark visited function, moved get places to main, added error checking to add place function, need to add sorting by unvisited marker and fix add place function.

--- travel_tracker.py
+++ travel_tracker.py
@@ -12,33 +12,8 @@ FILENAME = "places.csv"
 def main():
     """ """
     print("Travel Tracker 1.0 - by Rhys Simpson")
-    in_file = open(FILENAME, "r")
-    number = len(list(in_file))
-    print("{} places loaded from places.csv".format(number))
-    menu = "Menu:\nL - List Places\nA - Add new place\nM - Mark a place as visited\nQ - Quit"
-    print(menu)
-    choice = input(">>> ").upper()
-    while choice != "Q":
-        if choice == "L":
-            places = get_places()
-            print_places(places)
-        elif choice == "A":
-            places = get_places()
-            add_place(places)
-        elif choice == "M":
-            places = get_places()
-            mark_visited(places)
-        else:
-            print("Invalid Menu Choice")
-        print(menu)
-        choice = input(">>> ").upper()
-    print("Have a nice day")
-
-
-def get_places():
-    """ """
     places = []
-    in_file = open(FILENAME)
+    in_file = open(FILENAME, "r")
     for line in in_file:
         line = line.strip()
         parts = line.split(",")
@@ -46,7 +21,23 @@ def get_places():
         places.append(parts)
         places.sort(key=itemgetter(3, 2))
     in_file.close()
-    return places
+    number = len(list(places))
+    print("{} places loaded from places.csv".format(number))
+    menu = "Menu:\nL - List Places\nA - Add new place\nM - Mark a place as visited\nQ - Quit"
+    print(menu)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            print_places(places)
+        elif choice == "A":
+            add_place(places)
+        elif choice == "M":
+            mark_visited(places)
+        else:
+            print("Invalid Menu Choice")
+        print(menu)
+        choice = input(">>> ").upper()
+    print("Have a nice day")
 
 
 def print_places(places):
@@ -89,26 +80,26 @@ def add_place(places):
 def get_valid_input():
     """ """
     name = input("Name: ")
-    while name != "":
-        name = input("Name: ")
-    else:
+    while name == "":
         print("Input can not be blank")
-    name = input("Name: ")
-
+        name = input("Name: ")
     country = input("Country: ")
-    if country == "":
+    while country == "":
         print("Input can not be blank")
         country = input("Country: ")
-
-    priority = int(input("Priority: "))
-    if priority < 0:
-        print("Number must be > 0")
-        priority = int(input("Priority: "))
-
-    visited = "n"
-
-    print("{} in {} (priority {}) added to Travel Tracker".format(name, country, priority))
-    return name, country, priority, visited
+    finished = False
+    while not finished:
+        try:
+            priority = int(input("Priority: "))
+            while priority < 0:
+                print("Number must be > 0")
+                priority = int(input("Priority: "))
+            finished = True
+            VISITED = "n"
+            print("{} in {} (priority {}) added to Travel Tracker".format(name, country, priority))
+            return name, country, priority, VISITED
+        except ValueError:
+            print("Invalid input; enter a valid number")
 
 
 def mark_visited(places):
