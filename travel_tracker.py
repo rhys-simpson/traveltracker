239f47563b13ab663e6aae939fb17823de0c28c3
rhys-simpson <rhys030102@gmail.com>
2020-09-06 19:30:58 +1000

    added formatting to match sample output.

--- travel_tracker.py
+++ travel_tracker.py
@@ -8,15 +8,13 @@ from operator import itemgetter
 FILENAME = "places.csv"
 
 
-# TODO add print statements; formatting etc. to match sample output
 def main():
-    """ """
+    """Program to get and list users visited and unvisited places"""
     print("Travel Tracker 1.0 - by Rhys Simpson")
     places = get_places()
-    number = len(list(places))
-    print("{} places loaded from places.csv".format(number))
-    menu = "Menu:\nL - List Places\nA - Add new place\nM - Mark a place as visited\nQ - Quit"
-    print(menu)
+    print("{} places loaded from places.csv".format(len(list(places))))
+    MENU = "Menu:\nL - List Places\nA - Add new place\nM - Mark a place as visited\nQ - Quit"
+    print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
@@ -26,21 +24,14 @@ def main():
         elif choice == "M":
             mark_visited(places)
         else:
-            print("Invalid Menu Choice")
-        print(menu)
+            print("Invalid menu choice")
+        print(MENU)
         choice = input(">>> ").upper()
     save_places(places)
 
 
-def change_visited_number(places):
-    visit = 0
-    for places_data in places:
-        if places_data[3] == "n":
-            visit += 1
-    return visit
-
-
 def get_places():
+    """Convert input file to list"""
     places = []
     in_file = open(FILENAME, "r")
     for line in in_file:
@@ -54,13 +45,14 @@ def get_places():
 
 
 def print_places(places):
-    """ """
-
+    """Print places from place list"""
+    # Get longest elements in place list from another function
     longest_country_name_length, longest_number_length, longest_town_name_length = longest_elem_length(places)
 
-    visit = change_visited_number(places)
+    unvisited_number = unvisited_places_value(places)
     places.sort(key=itemgetter(3, 2))
     for number, places_data in enumerate(places):
+        # Check if place is unvisited and assign "*" in print statement
         if places_data[3] == "v":
             unvisited = ""
         else:
@@ -69,29 +61,38 @@ def print_places(places):
                                                                 longest_town_name_length, places_data[1],
                                                                 longest_country_name_length, places_data[2],
                                                                 longest_number_length))
-    if visit <= 0:
+    if unvisited_number <= 0:
         print("{} places. No places left to visit. Why not add a new place?".format(len(list(places))))
     else:
-        print("{} places. You still want to visit {} places.".format(len(list(places)), visit))
+        print("{} places. You still want to visit {} places.".format(len(list(places)), unvisited_number))
 
 
 def longest_elem_length(places):
-    """ """
+    """Get longest elements in places list for formatting"""
     longest_town_name_length = max(len(places_data[0]) for places_data in places)
     longest_country_name_length = max(len(places_data[1]) for places_data in places)
     longest_number_length = max(len(str(places_data[2])) for places_data in places)
     return longest_country_name_length, longest_number_length, longest_town_name_length
 
 
+def unvisited_places_value(places):
+    """Get the number of unvisited places"""
+    unvisited_places = 0
+    for places_data in places:
+        if places_data[3] == "n":
+            unvisited_places += 1
+    return unvisited_places
+
+
 def add_place(places):
-    """ """
+    """Add new place to places list"""
     name, country, priority = get_valid_input()
     new_place = [name, country, priority, "n"]
     places.append(new_place)
 
 
 def get_valid_input():
-    """ """
+    """Error checking for add new place function"""
     finished = False
     name = input("Name: ")
     while name == "":
@@ -116,16 +117,16 @@ def get_valid_input():
 
 
 def mark_visited(places):
-    """ """
+    """Code to mark an unvisited place as visited with error checking"""
     finished = False
-    visit = change_visited_number(places)
+    # Check if all places are visited
+    visit = unvisited_places_value(places)
     if visit == 0:
         print("No unvisited places")
-        finished = True
-
+        return
+    print_places(places)
+    print("Enter the number of a place to mark as visited")
     while not finished:
-        print_places(places)
-        print("Enter the number of a place to mark as visited")
         try:
             item_to_change = int(input(">>> "))
             if item_to_change < 0:
@@ -144,10 +145,11 @@ def mark_visited(places):
 
 
 def save_places(places):
-    """ """
+    """Save and update new places to csv"""
     out_file = open("places.csv", "w")
     for places_data in places:
         print("{},{},{},{}".format(places_data[0], places_data[1], places_data[2], places_data[3]), file=out_file)
+        places.sort(key=itemgetter(3))
     out_file.close()
     print("{} places saved to places.csv".format(len(list(places))))
     print("Have a nice day :)")
